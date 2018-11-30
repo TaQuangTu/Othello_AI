83a935f17b0aeeb67bf71628a11540b83a2d2cd8
Ta quang tu <taquangtu2017@gmail.com>
2018-11-30 17:04:51 +0700

    update waiting dialog

--- app/src/main/java/helloworld/com/taquangtu132gmail/taquangtu/ai/view/WaitingDialog.java
+++ app/src/main/java/helloworld/com/taquangtu132gmail/taquangtu/ai/view/WaitingDialog.java
@@ -4,7 +4,6 @@ import android.app.Dialog;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.view.Window;
 
 import helloworld.com.taquangtu132gmail.taquangtu.ai.R;
 
@@ -12,12 +11,11 @@ public class WaitingDialog extends Dialog {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.dialog_waiting);
-        this.setCancelable(false);
+        setCancelable(false);
     }
     public WaitingDialog(@NonNull Context context) {
-        super(context);
+        super(context,R.style.myDialog);
     }
 
 }
